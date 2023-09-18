16bcf40bac9983659e3284f0ef7d56328498cc9e
gegaabzianidze <gega.abzianidze.1@btu.edu.ge>
2023-09-18 19:05:47 +0400

    ENV

--- src/SupabaseClient.jsx
+++ src/SupabaseClient.jsx
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://fgiysdvqjpwtpzuipjlw.supabase.co";
-const supabaseAnonKey =
-"  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnaXlzZHZxanB3dHB6dWlwamx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE0ODgzMTQsImV4cCI6MjAwNzA2NDMxNH0.phxCCs2lUTLix3Lz43Lkow-SeWK-ylBzrhh1YMa4MRA";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey =import.meta.env.VITE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
